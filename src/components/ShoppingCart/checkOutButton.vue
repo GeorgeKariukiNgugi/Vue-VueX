d2091561786784fd37daa9e15a1cdde5addd2f6e
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-06-13 08:24:31 +0300

    Added block checkout

--- src/components/ShoppingCart/checkOutButton.vue
+++ src/components/ShoppingCart/checkOutButton.vue
@@ -1,8 +1,8 @@
 <template>
   <div>    
       <v-row justify center class="mb-12">
-        <v-col  md="4">
-            <v-btn color="success" block>Check Out ($$$$)</v-btn>
+        <v-col  md="12">
+            <v-btn block color="success" >Check Out ($$$$)</v-btn>
         </v-col>        
       </v-row>        
   </div>
