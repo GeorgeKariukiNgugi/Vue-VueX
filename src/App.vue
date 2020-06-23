4da5c6f71a068e7fe8745de2401fce8ee62dfa9c
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-06-23 15:51:53 +0300

    Added The Single Product Page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar      
-      app
-      class="black--text"
-      color="#BBE6D6"
-      dark
-      style="margin:0%"
-    >
+    <v-app-bar app class="black--text" color="#BBE6D6" dark style="margin:0%">
       <v-app-bar-nav-icon
         color="black"
         @click="drawer = !drawer"
@@ -38,7 +32,7 @@
         <v-icon color="black">email</v-icon>
       </v-btn>
       <router-link to="/products">
-        <v-badge v-show="show" color="green" content="6" overlap>
+        <v-badge v-show="show" color="green" content="" overlap>
           <v-btn icon color="#BBE6D6">
             <v-icon color="black">mdi-cart</v-icon>
           </v-btn>
@@ -46,25 +40,16 @@
       </router-link>
     </v-app-bar>
     <v-navigation-drawer v-model="drawer" app color="#BBE6D6">
-      <!-- <v-list-item-content>
-        <v-list-item-title class="title">
-          Application
-        </v-list-item-title>
-        <v-list-item-subtitle>
-          subtext
-        </v-list-item-subtitle>
-      </v-list-item-content> -->
-
       <v-list-item two-line :class="miniVariant && 'px-0'">
-            <v-list-item-avatar>
-              <img src="https://randomuser.me/api/portraits/men/81.jpg">
-            </v-list-item-avatar>
+        <v-list-item-avatar>
+          <img src="https://randomuser.me/api/portraits/men/81.jpg" />
+        </v-list-item-avatar>
 
-            <v-list-item-content>
-              <v-list-item-title>Application</v-list-item-title>
-              <v-list-item-subtitle>Subtext</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>Application</v-list-item-title>
+          <v-list-item-subtitle>Subtext</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
 
       <v-divider></v-divider>
       <v-list nav dense app>
@@ -95,13 +80,14 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-content class="my-5">
+    <v-content  class="my-5">
       <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
   name: "App",
   methods: {
@@ -110,16 +96,22 @@ export default {
       this.searchInput = !this.searchInput;
     },
   },
+  computed: {
+    ...mapGetters(["productItems", "numberInPagination","cartItems"]),
+  },
   // components: { ProductList, Cart },
   data: () => ({
     drawer: true,
     show: true,
-    searchInput: false,
+    searchInput: false,    
   }),
+  // updated(){
+
+  // }
 };
 </script>
-<style >
- a {
-    text-decoration: none;
+<style>
+a {
+  text-decoration: none;
 }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,84 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div color="grey">
+    <v-row no-gutters class="my-8">
+      <v-col md="6" offset-md="3">
+        <v-card class="v-card--material pa-3">
+          <v-row>
+            <v-col align="center">
+              <div class="d-flex grow flex-wrap">
+                <v-avatar
+                  size="128"
+                  class="mx-auto v-card--material-avatar elevation-6"
+                  color="grey"
+                >
+                  <img
+                    src="https://cdn.vuetifyjs.com/images/john.jpg"
+                    alt="John"
+                  />
+                </v-avatar>
+              </div>
+              <h2>This is the Name Of The Product.</h2>
+              <h3 class="font-weight-regular">Price: 300/=</h3>
+              <h3 class="font-weight-regular">Discount: 1%</h3>
+              <h2 blue>New Price: 250 /=</h2>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <h3 align="center">Description</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident voluptas veniam voluptatibus ipsum, asperiores beatae
+                animi, laboriosam voluptatum similique eos enim excepturi
+                quaerat vel assumenda unde odio amet inventore officiis!
+              </p>
+              <h3 align="center">Rating</h3>
+              <v-rating align="center" v-model="rating"></v-rating>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <h3 align="center">Reviews</h3>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col md="6" offset-md="3">
+              <v-btn
+                block
+                color="green"
+                class="ma-2 white--text"
+                large
+                dark="true"
+              >
+                Add To Cart
+                <v-icon right dark>mdi-cart</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    rating: 3,
+  }),
+};
+</script>
+
+<style>
+.v-card--material-avatar {
+  position: relative;
+  top: -64px;
+  margin-bottom: -32px;
+}
+.v-card--material-heading {
+  position: relative;
+  top: -40px;
+  transition: 0.3s ease;
+  z-index: 1;
+}
+</style>
